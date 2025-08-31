77fea797c6e5eb9400caa9e4af8cdb92ef7bcc53
bindhuhk <hkbindhu@gmail.com>
2025-08-31 23:25:45 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -5,6 +5,6 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id="hkbindhu/Tourism-Package-Prediction",  # the target repo
-    repo_type="model",                            # dataset, model, or space
+    repo_type="space",                            # dataset, model, or space
     path_in_repo="",                              # optional: subfolder path inside the repo
 )
