451c9f22fe5549a29b418689ffc0dc99880889cd
bindhuhk <hkbindhu@gmail.com>
2025-08-31 23:25:09 +0530

    Update data_register.py

--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -10,14 +10,7 @@ repo_id = "hkbindhu/Tourism-Package-Prediction"  # Update with your Hugging Face
 repo_type = "dataset"
 folder_path = "tourism_project/data"
 
-# Check authentication
-try:
-    user = api.whoami()
-    print("✅ Successfully authenticated as:", user["name"])
-except Exception as e:
-    print("❌ Authentication failed:", e)
-
-# Step 1: Check if the space exists or create it
+# Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
     print(f"Space '{repo_id}' already exists. Using it.")
@@ -26,11 +19,8 @@ except RepositoryNotFoundError:
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
 
-# Upload the data folder to Hugging Face dataset repo
 api.upload_folder(
-    folder_path=folder_path,
+    folder_path="tourism_project/data",
     repo_id=repo_id,
     repo_type=repo_type,
-    path_in_repo="data"
 )
-print("✅ File uploaded!")
